095b9f9602251edeaa6ca45a0b45dfe6f40f4ee4
Lauri Laaksonen <laaksonel@gmail.com>
2023-10-06 18:35:38 +0300

    Move stuff to lib

--- lib/default.nix
+++ lib/default.nix
@@ -1,3 +1,68 @@
-{
+{ pkgs, ... }:
 
+let
+  myConfig = pkgs.vimUtils.buildVimPlugin {
+    name = "My Neovim config";
+    src = ../lua;
+  };
+
+  # Add to finalConficRC
+  # require 'lauri'.init()
+  finalConfigRC = ''
+    lua << EOF
+      print "FOO"
+    EOF
+  '';
+in
+rec {
+  neovim = pkgs.wrapNeovim pkgs.neovim-unwrapped {
+    viAlias = true;
+    vimAlias = true;
+    configure = {
+      customRC = finalConfigRC;
+
+      packages.myVimPackage = {
+        start = with pkgs.vimPlugins; [
+          # lsp
+          nvim-lspconfig
+          nvim-cmp
+          cmp-nvim-lsp
+          lspkind-nvim
+
+          luasnip
+          cmp_luasnip
+
+          nvim-treesitter.withAllGrammars
+          nvim-treesitter-context
+
+          nvim-notify
+          lualine-nvim
+          gitsigns-nvim
+
+          # theme
+          catppuccin-nvim
+
+          # telescope
+          plenary-nvim
+          popup-nvim
+          telescope-nvim
+          telescope-file-browser-nvim
+          telescope-fzf-native-nvim
+          tmux-nvim
+
+          vim-nix
+          vim-fugitive
+
+          harpoon
+          nerdcommenter
+
+          indent-blankline-nvim
+          tabular
+          wilder-nvim
+
+          myConfig
+        ];
+      };
+    };
+  };
 }
