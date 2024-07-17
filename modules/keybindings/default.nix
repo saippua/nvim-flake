77250a288c1a6ddfae289a7cae5c715bfbaea550
Saippua <ollikos92@gmail.com>
2024-07-17 17:23:03 +0300

    added tab switch keybindings

--- modules/keybindings/default.nix
+++ modules/keybindings/default.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
-
 {
   vim.luaConfigRC = /* lua */ ''
     vim.keymap.set('n', '<leader>pv', ":Telescope file_browser path=%:p:h<CR>", {silent = true, noremap = true })
+    vim.keymap.set('n', '<C-L>', ":tabnext", {silent = true, noremap = true })
+    vim.keymap.set('n', '<C-H>', ":tabprevious", {silent = true, noremap = true })
   '';
 }
