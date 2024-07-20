93ee12a357de14c69323568788ec083bfa8dd3fa
Saippua <ollikos92@gmail.com>
2024-07-20 11:30:22 +0300

    added switch tab hotkey

--- modules/keybindings/default.nix
+++ modules/keybindings/default.nix
@@ -2,7 +2,7 @@
 {
   vim.luaConfigRC = /* lua */ ''
     vim.keymap.set('n', '<leader>pv', ":Telescope file_browser path=%:p:h<CR>", {silent = true, noremap = true })
-    vim.keymap.set('n', '<C-L>', ":tabnext", {silent = true, noremap = true })
-    vim.keymap.set('n', '<C-H>', ":tabprevious", {silent = true, noremap = true })
+    vim.keymap.set('n', '<C-S-W><C-S-L>', ":tabnext<CR>", {silent = true, noremap = true })
+    vim.keymap.set('n', '<C-S-W><C-S-H>', ":tabprevious<CR>", {silent = true, noremap = true })
   '';
 }
