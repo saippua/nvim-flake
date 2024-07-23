a8e94a08d9eb752a294d5f36eab3c8ff6dd5c115
Saippua <ollikos92@gmail.com>
2024-07-23 20:17:51 +0300

    added clangd lsp

--- modules/lsp/default.nix
+++ modules/lsp/default.nix
@@ -53,7 +53,7 @@
           -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', "<cmd>lua require('telescope.builtin').lsp_references()<CR>", opts)
           -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gI', "<cmd>lua require('telescope.builtin').lsp_implementations()<CR>", opts)
           vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wd', "<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>", opts)
-          vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>h', function() vim.cmd("ClangdSwitchSourceHeader") end)
+          -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>h', function() vim.cmd("ClangdSwitchSourceHeader") end)
           -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>vf', "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
           -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>vd', "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 
@@ -142,6 +142,15 @@
         capabilities = capabilities;
         on_attach = function(client, bufnr)
           attach_keymaps(client,bufnr)
+        end,
+      }
+
+      lspconfig.clangd.setup {
+        on_init = custom_init;
+        capabilities = capabilities;
+        on_attach = function(client, bufnr)
+          attach_keymaps(client, bufnr)
+          vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>h', function() vim.cmd("ClangdSwitchSourceHeader") end)
         end,
       }
     '';
