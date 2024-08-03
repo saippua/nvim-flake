a44bd169a7fda77d8d91f8864aaf7a683d61a38a
Saippua <ollikos92@gmail.com>
2024-08-03 22:13:37 +0300

    fixed query driver for clangd

--- modules/lsp/default.nix
+++ modules/lsp/default.nix
@@ -103,7 +103,7 @@
           "--background-index",
           "--clang-tidy",
           "--completion-style=bundled",
-          "--query-driver=/nix/store/n356cbpp74lkscr6njiwakj4flyndff6-xtensa-esp32-elf-esp-idf-v5.1.3/bin/xtensa-esp32-elf-*",
+          "--query-driver=/nix/store/*-xtensa-esp32-elf-esp-idf-v5.1.4/bin/xtensa-esp32-elf-*",
         }
       }
     '';
